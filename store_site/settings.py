b9f46a31bafbabdcf2a02a17c03d7495c03f3368
marufbinsalim <mdmarufbinsalim@gmail.com>
2024-12-19 00:09:18 +0600

    updated

--- store_site/settings.py
+++ store_site/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,15 +85,14 @@ WSGI_APPLICATION = 'store_site.wsgi.application'
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'defaultdb',
-        'USER': 'avnadmin',
-        'PASSWORD': 'AVNS_aofHNttgjaNqVCaJpua',
-        'HOST': 'mysql-25562ba5-allprojects94-0a43.j.aivencloud.com',
-        'PORT': '22971',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
+        'NAME': os.getenv('DB_NAME', 'defaultdb'),
+        'USER': os.getenv('DB_USER', 'avnadmin'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'AVNS_aofHNttgjaNqVCaJpua'),
+        'HOST': os.getenv('DB_HOST', 'mysql-25562ba5-allprojects94-0a43.j.aivencloud.com'),
+        'PORT': os.getenv('DB_PORT', '22971'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
